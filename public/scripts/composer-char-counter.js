78c302431872b54b33f515f0820d17a0be6ea3b2
Anshu Pasrija <anshu.pasrija@gmail.com>
2021-09-09 22:10:35 -0700

    tweeter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,12 +3,10 @@ $(document).ready(function () {
   console.log("JQuery is ready");
   const maxLength = 140;
   $("#tweet-text").on("keyup", function () {
-    const textLength = this.value.length;
-    console.log("textLength",textLength);
+    const textLength = this.value.length;   
     const counterValue = maxLength - textLength;
     const counter = $("#tweet-text,.counter").text(counterValue);
     if (counterValue < 0) {
-      console.log("i am in ");
       counter.css("color", "red");
     } else {
       counter.css("color", "black");
